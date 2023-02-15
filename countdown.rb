aeb4adca100697e5df0bf9d508c39de3fb885459
Isaac Ongera <isaacongera717@gmail.com>
2023-02-15 16:00:39 +0300

    completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
+# require 'pry'
 
-def countdown
+def countdown(number)
+    while number > 0
+        
+        puts "#{number} SECOND(S)!"
+        number-=
+        sleep 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+
+
+def countdown_with_sleep(number)
+    while number > 0
+        
+        puts "#{number} SECOND(S)!"
+        number-=
+        sleep 1
+    end
+    "HAPPY NEW YEAR!"
 end
